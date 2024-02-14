76d36a636e68d74e0bd856e80349760d378b3fe5
furkanselvi96 <furkan-selvi-1996@hotmail.com>
2024-02-14 22:00:20 +0300

    TableDetail.java -> updated.

--- src/main/java/com/fuse/coffeemanagement/model/TableDetail.java
+++ src/main/java/com/fuse/coffeemanagement/model/TableDetail.java
@@ -1,16 +1,20 @@
 package com.fuse.coffeemanagement.model;
 
-
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tables")
+@Getter
+@Setter
 public class TableDetail {
 
-    @Id
-    private Long id;
+    @EmbeddedId
+    private TableDetailId tableDetailId;
 
     @Column(name = "table_status", nullable = false)
     private boolean tableStatus;
@@ -25,11 +29,37 @@ public class TableDetail {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TableDetail that = (TableDetail) o;
-        return tableStatus == that.tableStatus && Objects.equals(id, that.id);
+        return tableStatus == that.tableStatus && Objects.equals(tableDetailId, that.tableDetailId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, tableStatus);
+        return Objects.hash(tableDetailId, tableStatus);
+    }
+
+    @Embeddable
+    @Getter
+    @Setter
+    public static class TableDetailId implements Serializable {
+
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private Long id;
+
+        @Column(name = "table_name", nullable = false)
+        private String name;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TableDetailId that = (TableDetailId) o;
+            return Objects.equals(id, that.id) && Objects.equals(name, that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
     }
 }
